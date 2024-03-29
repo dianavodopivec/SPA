10a10a0390c56604c829c274f541fe6c8ce6bfe8
Diana <dianavodopivec16@gmail.com>
2024-03-29 13:07:31 -0300

    SPA: AJAX Request Helper

--- app/App.js
+++ app/App.js
@@ -1,7 +1,24 @@
-import api from "./helpers/wp_api"
+import api from "./helpers/wp_api.js"
+import {ajax} from "./helpers/ajax.js";
 
 export function App() {
     document.getElementById("root").innerHTML = `<h1>Hola</h1>`
 
     console.log(api)
+
+    //Ultimos post
+    ajax({
+        url: api.POST,
+        cbSuccess: (posts) => {
+            console.log(posts)
+        }
+    })
+
+    //Categorias
+
+    ajax({
+        url: api.CATEGORIES,
+        cbSuccess: categories => console.log(categories)
+    })
 }
+
--- app/helpers/wp_api.js
+++ app/helpers/wp_api.js
@@ -1,12 +1,13 @@
 //Guardamos variables estáticas que nos permiten consultar a un dominio de Wordpress.
 
-const NAME = "malvestida";
-const DOMAIN = `https://${NAME}.com`
-const SITE = `${DOMAIN}/wp-json`
-const API_WP = `${SITE}/wp/v2`
-const POSTS = `${API_WP}/posts?_embed`
-const POST = `${API_WP}/posts`
-const SEARCH = `${API_WP}/search?_embed&search=`
+const NAME = "css-tricks",
+DOMAIN = `https://${NAME}.com`,
+SITE = `${DOMAIN}/wp-json`,
+API_WP = `${SITE}/wp/v2`,
+POSTS = `${API_WP}/posts?_embed`,
+POST = `${API_WP}/posts`,
+CATEGORIES = `${API_WP}/categories`,
+SEARCH = `${API_WP}/search?_embed&search=`;
 
 export default {
     NAME,
@@ -15,5 +16,6 @@ export default {
     API_WP,
     POSTS,
     POST,
+    CATEGORIES,
     SEARCH
 }
