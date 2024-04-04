e309600df377a016520806a4d1b9ce02d196373b
Diana <dianavodopivec16@gmail.com>
2024-04-03 21:12:31 -0300

    SPA: Asynchronous routing

--- app/components/Router.js
+++ app/components/Router.js
@@ -4,33 +4,35 @@ import {ajax} from "../helpers/ajax.js";
 import {PostCard} from "./PostCard.js";
 
 //Router -> Invoca las peticiones.
-export function Router() {
+export async function Router() {
     let {hash} = location;
-    const $posts = document.getElementById("main")
+    const $main = document.getElementById("main")
     console.log(hash)
 
-    $posts.innerHTML = null //Igualamos el HTML a null para que la carga de su contenido cargue limpiamente ()
+    $main.innerHTML = null //Igualamos el HTML a null para que la carga de su contenido cargue limpiamente ()
 
     //Si el hash no existe o #/ tampoco, se hará referencia a la Home.
     if(!hash || hash === "#/") {
-        ajax({
+        await ajax({
             url: api.POST,
             cbSuccess: (posts) => {
                 //console.log(posts)
                 let html = "" //Inicialización vacía
                 posts.forEach(post => {
                     html += PostCard(post) //Almacenamiento del código HTML de cada POST para formar la estructura de la postcard.
-                    const cargador = document.querySelector(".cargador");
-                    cargador.style.display = "none"
-                    $posts.innerHTML = html;
+                    $main.innerHTML = html;
                 })
             }
         })        //Si encuentra el fragmento entre (), validará a true y en caso contrario false.
     } else if (hash.includes("#/search")) { 
-        $posts.innerHTML = "<h2> Sección de Buscador </h2>"
+        $main.innerHTML = "<h2> Sección de Buscador </h2>"
     } else if (hash === "#/contacto") { 
-        $posts.innerHTML = "<h2> Sección de Contacto </h2>"
+        $main.innerHTML = "<h2> Sección de Contacto </h2>"
     } else {
-        $posts.innerHTML = "<h2> Carga del contenido del Post previamente seleccionado </h2>"
+        $main.innerHTML = "<h2> Carga del contenido del Post previamente seleccionado </h2>"
     }
+
+    //Loader desaparece en las distintas páginas
+    const cargador = document.querySelector(".cargador");
+    cargador.style.display = "none"
 }
--- app/helpers/ajax.js
+++ app/helpers/ajax.js
@@ -1,12 +1,14 @@
-export function ajax (params) {
+export async function ajax (params) {
     const {url, cbSuccess} = params;
-    fetch(url)
+    
+    await fetch(url)
     .then(res => res.ok ? res.json() : Promise.reject(res)) //Si la promesa NO se cumple.
     .then(json => cbSuccess(json)) //Si la promesa SI se cumple.
     .catch(error => { //Manejo de errores
         const mensaje = error.satusText || "Ocurrió un error al acceder a la API ☹️"
 
-        document.getElementById("posts").innerHTML = `<div>
+        document.getElementById("main").innerHTML = 
+        `<div>
         <p>Error ${error.status}: ${mensaje}</p>
         </div>`
 
