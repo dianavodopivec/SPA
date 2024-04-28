32dfd79af3d13b7ea2cddbd3473ba5bc182aa8e1
Diana <dianavodopivec16@gmail.com>
2024-04-28 17:56:07 -0300

    SPA: current search view

--- app/helpers/wp_api.js
+++ app/helpers/wp_api.js
@@ -4,18 +4,23 @@ const NAME = "malvestida",
 DOMAIN = `https://${NAME}.com`,
 SITE = `${DOMAIN}/wp-json`,
 API_WP = `${SITE}/wp/v2`,
-POSTS = `${API_WP}/posts?_embed`,
+PER_PAGE = 5,
+POSTS = `${API_WP}/posts?_embed&per_page=${PER_PAGE}`,
 POST = `${API_WP}/posts`,
-CATEGORIES = `${API_WP}/categories`,
-SEARCH = `${API_WP}/search?_embed&search=`;
+//CATEGORIES = `${API_WP}/categories`,
+SEARCH = `${API_WP}/search?_embed&per_page=${PER_PAGE}&search=`;
+
+let page = 1;
 
 export default {
     NAME,
     DOMAIN,
     SITE,
     API_WP,
+    PER_PAGE,
     POSTS,
     POST,
-    CATEGORIES,
-    SEARCH
+    //CATEGORIES,
+    SEARCH,
+    page
 }
