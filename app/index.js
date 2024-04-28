6f2fbc20205424ac5154174ef872f653b618c886
Diana <dianavodopivec16@gmail.com>
2024-04-28 19:07:16 -0300

    SPA: Infinite scroll & High Order Components

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
+import api from "./helpers/wp_api.js"
 import {App} from "./App.js";
 
 document.addEventListener("DOMContentLoaded", App);
-window.addEventListener("hashchange", App)
+//Cada vez que se detecte un cambio en el hash, se ejecutará el componente app y el valor de api.page volverá a 1
+window.addEventListener("hashchange", () => {
+    api.page = 1
+    App()
+})
